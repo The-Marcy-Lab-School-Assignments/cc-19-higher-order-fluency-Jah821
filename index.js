a9ab40f449f7eb645d5f09a9c6d0df5e6eff523b
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-12-14 16:02:29 +0000

    finshed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-//write your code here
+const alumni = [
+{name:'Jarrit', job:'TPT',language:'JavaScript', age:22}, 
+{name:'Stephanie', job:'JPMorgan',language:'JavaScript', age:21}, 
+{name:'Devonte', job:'WW',language:'JavaScript', age:23}, 
+{name:'Enmanuel', job:'Asana',language:'JavaScript', age:21},
+{name:'Shemar', job:'SquareSpace',language:'JavaScript', age:23},
+{name:'Cielo', job:'NYT',language:'JavaScript', age:21}]
+
+ 
+ const averageAge = (array) => {
+    const sum = array.reduce((a,b) => a + b.age, 0);
+    return Math.floor(sum/ array.length);
+};
+
+
+ const bananaBread = array => {
+     return array.map(alum => {
+         return {...alum, bananaBread : `${alum.name} got banana bread at ${alum.job}` };
+     });
+ };
+
+const ninetiesBabies =(alumni)=> alumni.filter(c => c.age > 21);
+
+console.log(ninetiesBabies(alumni));
+
+const addSchool = array => {
+    return array.map(alum => {
+        return {... alum, school : "The Marcy Lab School"};
+    });
+};
+
+
+const allUseJavaScript = array => {
+    return array.every(obj => {
+        return obj.language === "JavaScript";
+    });
+};
+
+console.log(allUseJavaScript(alumni))
